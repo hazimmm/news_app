af7752600d9efb4dd05652e59e0ec4a451abde63
HaIz <92291837+hazimmm@users.noreply.github.com>
2023-06-26 03:40:01 +0800

    add onTap at search Page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:news/screens/profile.dart';
-import 'package:news/screens/News_Page_screen.dart';
+import 'News_Page_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -117,10 +117,10 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16, 25, 16, 0),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(16, 25, 16, 0),
               child: Row(
-                children: const [
+                children: [
                   Text(
                     'Top Headlines',
                     style: TextStyle(
@@ -231,6 +231,17 @@ class _SearchPageState extends State<SearchPage> {
     }
   }
 
+  void navigateToNewsPage(int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => NewsPage(
+          newsId: index.toString(),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -271,22 +282,23 @@ class _SearchPageState extends State<SearchPage> {
                 itemBuilder: (context, index) {
                   final article = searchResults[index];
 
-                  return Card(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 16.0,
-                      vertical: 8.0,
-                    ),
-                    child: ListTile(
-                      title: Text(
-                        article['title'],
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
+                  return GestureDetector(
+                    onTap: () => navigateToNewsPage(index),
+                    child: Card(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 16.0, vertical: 8.0),
+                      child: ListTile(
+                        title: Text(
+                          article['title'],
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      subtitle: Text(
-                        article['source']['name'],
-                        style: const TextStyle(
-                          fontStyle: FontStyle.italic,
+                        subtitle: Text(
+                          article['source']['name'],
+                          style: const TextStyle(
+                            fontStyle: FontStyle.italic,
+                          ),
                         ),
                       ),
                     ),
@@ -300,3 +312,4 @@ class _SearchPageState extends State<SearchPage> {
     );
   }
 }
+
