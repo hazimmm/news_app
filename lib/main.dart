d32cbfffc5109b8845764748d01e6a57b4232673
Luqman2428 <129022130+Luqman2428@users.noreply.github.com>
2023-06-25 23:29:22 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:news/screens/News_Page_screen.dart';
 import 'package:news/screens/signin_screen.dart';
 import 'package:news/screens/profile.dart';
 import 'package:news/screens/home_screen.dart';
-
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -14,18 +13,23 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  get newsId => null;
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       initialRoute: '/login',
     routes: {
       '/login': (context) => const SignInScreen(), // Home route (sign-in page)
-      '/profile': (context) => ProfilePage(), // Profile rout
-      '/home': (context) => const HomeScreen(), // HomeScreen rout
+      '/profile': (context) => const ProfilePage(), // Profile rout
+      '/home': (context) => const HomeScreen(),
+      '/news_page/:newsId': (context) => NewsPage(newsId: ModalRoute.of(context)?.settings.arguments as String),
+
 
     }
     );
